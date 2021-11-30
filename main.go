48e97673e7034b2fb358be5bcab19cbb3d0fbe6b
jimwoodward <jimwoodward15@gmail.com>
2021-11-30 08:19:14 -0500

    clean up

--- main.go
+++ main.go
@@ -6,14 +6,12 @@ import (
 
 // Pseudo code:
 // 1. Iterate through each byte of the input string.
-// 2. If there is a byte past the ith position (if i is not the last byte in the string)...
-// 3. If that preceding byte is greater than the ith byte (criteria #1 for a valid roman numeral combo)...
-// 4. If the combination of the two bytes is a valid roman numeral combination i.e. "CD", "IX", etc., then add the
+// 2. If there is a byte past the i position (if i is not the last byte in the string)...
+// 4. If the combination of the i and i+1 bytes is a valid roman numeral combination i.e. "CD", "IX", etc., then add the
 // corresponding value to the returned value and iterate i by 1 (since we have just evaluated the preceding byte we need
 // to skip over it)
-// 4. Else, then just add the corresponding value of the ith byte to the returned value
-// 5. Else, then just add the corresponding value of the ith byte to the returned value
-// 6. Else, then just add the corresponding value of the ith byte to the returned value and return
+// 4. Else, then just add the corresponding value of the i byte to the returned value
+// 6. Else, then just add the corresponding value of the i byte to the returned value and return
 // 7. Return
 
 func romanToInt(s string) int {
@@ -38,13 +36,9 @@ func romanToInt(s string) int {
 
 	for i := 0; i < lengthOfInput; i++ {
 		if i < lengthOfInput-1 {
-			if romanToInt[string(s[i])] < romanToInt[string(s[i+1])] {
-				if val, ok := romanToInt[string(s[i])+string(s[i+1])]; ok {
-					retVal += val
-					i += 1
-				} else {
-					retVal += romanToInt[string(s[i])]
-				}
+			if val, ok := romanToInt[string(s[i])+string(s[i+1])]; ok {
+				retVal += val
+				i += 1
 			} else {
 				retVal += romanToInt[string(s[i])]
 			}
